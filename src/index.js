259eabf1c139170080284c84f41859c2bc1201db
T-Marria <t.marria@mail.ru>
2023-07-13 00:18:56 +0400

    Added main variables and methods, added css file and modal window

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-console.log("Hello world")
-alert("Hello there, Obi Wan Kenobi")
+const btnOpen = document.querySelector("#btn-open");
+const btnClose = document.querySelector("#btn-close")
+const modal = document.querySelector("#modal");
+
+btnOpen.onclick = () => {
+    modal.showModal()
+}
+
+btnClose.onclick = () => {
+    modal.close()
+}
+
+
+function Contact(name, phone, isFavorite = false) {
+    this.name = name;
+    this.phone = phone;
+    this.isFavorite = isFavorite;
+}
+
+const contactList = {
+list: [],
+SortByName() {
+    this.list.sort((a, b) => a.name > b.name ? 1 : -1);
+},
+AddContact(newContact){
+    this.list.push(newContact);
+    this.SortByName();
+},
+//TODO: DeleteContact()
+};
+
+contactList.AddContact(new Contact("Marie", "89608091515", true))
+contactList.AddContact(new Contact("Leshenka", "89878171323", true))
+contactList.AddContact(new Contact("Alice", "89277125279"))
+
+console.log(contactList.list)
+
